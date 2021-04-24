561ca2064449946e14a17cc5ea47b3ec2aaa824d
EvModder <evmodder@gmail.com>
2021-04-24 00:37:52 -0700

    Fix (some of) the mis-aligned javadoc comments

--- src/net/evmodder/EvLib/Updater.java
+++ src/net/evmodder/EvLib/Updater.java
@@ -86,9 +86,12 @@ public class Updater {
 	 * Allows the developer to specify the type of update that will be run.
 	 */
 	public enum UpdateType{
-		DEFAULT,/** Run a version check, and then if the file is out of date, download the newest version. */
-		NO_VERSION_CHECK,/** Don't run a version check, just find the latest update and download it. */
-		NO_DOWNLOAD/** Get information about the version and the download size, but don't actually download anything. */
+		/** Run a version check, and then if the file is out of date, download the newest version. */
+		DEFAULT,
+		/** Don't run a version check, just find the latest update and download it. */
+		NO_VERSION_CHECK,
+		/** Get information about the version and the download size, but don't actually download anything. */
+		NO_DOWNLOAD
 	}
 	/**
 	 * Represents the various release types of a file on BukkitDev.
