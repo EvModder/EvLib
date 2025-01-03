a5e94b609b000accdd9a3f8851c59fb03cc998ee
EvModder <evmodder@gmail.com>
2025-01-02 23:49:23 -0600

    1.21.4 support (Record type for ResoveableProfile)

--- src/net/evmodder/EvLib/extras/HeadUtils.java
+++ src/net/evmodder/EvLib/extras/HeadUtils.java
@@ -2,6 +2,7 @@ package net.evmodder.EvLib.extras;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Optional;
 import java.util.UUID;
 import org.bukkit.Bukkit;
@@ -92,8 +93,8 @@ public final class HeadUtils {
 		}
 	}
 
-	private static Field fieldProfileItem, fieldProfileBlock;
-	private static Field fieldResolveableName, fieldResolveableId, fieldResolveableProperties;
+	// 1.21.1+ changed the field's type from GameProfile to ResolveableProfile
+
 	private static Constructor<?> resolveableProfileConstructor;
 	private static Object convertToResolvedProfile(GameProfile profile){
 		try{
@@ -106,33 +107,62 @@ public final class HeadUtils {
 		}
 		return profile;
 	}
+
+	private static Method[] resolveableProfileRecordAccessors;
+	private static Field fieldResolveableName, fieldResolveableId, fieldResolveableProperties;
+	@SuppressWarnings("unchecked")
 	private static GameProfile convertFromResolvedProfile(Object profile){
 		if(profile == null) return null;
 		try{
-			// 1.21.1+ changed the field's type from GameProfile to ResolveableProfile
-			if(fieldResolveableProperties == null){
+			if(resolveableProfileRecordAccessors == null && fieldResolveableProperties == null){
 				final Class<?> clazzResolveableProfile = Class.forName("net.minecraft.world.item.component.ResolvableProfile");
-				fieldResolveableName = clazzResolveableProfile.getDeclaredField("name");
-				fieldResolveableName.setAccessible(true);
-				fieldResolveableId = clazzResolveableProfile.getDeclaredField("id");
-				fieldResolveableId.setAccessible(true);
-				fieldResolveableProperties = clazzResolveableProfile.getDeclaredField("properties");
-				fieldResolveableProperties.setAccessible(true);
+				//if(clazzResolveableProfile.isInstance(profile)) printError = true;
+				try{
+					fieldResolveableName = clazzResolveableProfile.getDeclaredField("name");
+					fieldResolveableName.setAccessible(true);
+					fieldResolveableId = clazzResolveableProfile.getDeclaredField("id");
+					fieldResolveableId.setAccessible(true);
+					fieldResolveableProperties = clazzResolveableProfile.getDeclaredField("properties");
+					fieldResolveableProperties.setAccessible(true);
+				}
+				// 1.21.4+ changed ResolveableProfile class type to a Record
+				catch(NoSuchFieldException e){
+					//if((boolean)Class.class.getMethod("isRecord").invoke(clazzResolveableProfile)){
+					Object[] rcs = (Object[])Class.class.getMethod("getRecordComponents").invoke(clazzResolveableProfile);
+					Method getAccessor = rcs[0].getClass().getMethod("getAccessor");
+					resolveableProfileRecordAccessors = new Method[rcs.length];
+					for(int i=0; i<3; ++i) resolveableProfileRecordAccessors[i] = (Method)getAccessor.invoke(rcs[i]);
+				}
 			}
-			@SuppressWarnings("unchecked")
-			String name = ((Optional<String>)fieldResolveableName.get(profile)).orElse("");
-			@SuppressWarnings("unchecked")
-			UUID uuid = ((Optional<UUID>)fieldResolveableId.get(profile)).get();
-			PropertyMap properties = (PropertyMap)fieldResolveableProperties.get(profile);
+			final Optional<String> nameOptional;
+			final Optional<UUID> uuidOptional;
+			final PropertyMap properties;
+			if(resolveableProfileRecordAccessors != null){
+				nameOptional = (Optional<String>)resolveableProfileRecordAccessors[0].invoke(profile);
+				uuidOptional = (Optional<UUID>)resolveableProfileRecordAccessors[1].invoke(profile);
+				properties = (PropertyMap)resolveableProfileRecordAccessors[2].invoke(profile);
+			}
+			else{
+				nameOptional = (Optional<String>)fieldResolveableName.get(profile);
+				uuidOptional = (Optional<UUID>)fieldResolveableId.get(profile);
+				properties = (PropertyMap)fieldResolveableProperties.get(profile);
+			}
+			final String name = nameOptional.orElse("");
+			final UUID uuid = uuidOptional.orElseThrow(() -> new NullPointerException("UUID missing in ResolvedProfile"));
 			GameProfile gp = new GameProfile(uuid, name);
 			gp.getProperties().putAll(properties);
 			return gp;
 		}
 		catch(ReflectiveOperationException e){
 			Bukkit.getLogger().severe("DropHeads-HeadUtils: Unable to convert from ResolvedProfile");
+			/*if(printError) */e.printStackTrace();
+			fieldResolveableProperties = null;
+			resolveableProfileRecordAccessors = null;
 		}
 		return (GameProfile)profile;
 	}
+
+	private static Field fieldProfileItem, fieldProfileBlock;
 	public static void setGameProfile(SkullMeta meta, GameProfile profile){
 		try{
 			if(fieldProfileItem == null){fieldProfileItem = meta.getClass().getDeclaredField("profile");fieldProfileItem.setAccessible(true);}
